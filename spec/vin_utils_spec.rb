13ffcd43518b1f2f46a574fb379bbad40c27aede
Diego Stiehl <seccomiro@users.noreply.github.com>
2021-02-25 22:30:16 -0300

    Remove gem module spec

--- spec/vin_utils_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe VinUtils do
-  it 'has a version number' do
-    expect(VinUtils::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
-end
